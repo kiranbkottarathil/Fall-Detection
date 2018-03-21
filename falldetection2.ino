7705812542a791952a01107328950ae428c8ea5d
mamathapt <37322029+mamathapt@users.noreply.github.com>
2018-03-21 21:27:37 +0530

    Add files via upload

---
+++ falldetection2.ino
@@ -0,0 +1,44 @@
+
+#include<Wire.h>
+#include<SoftwareSerial.h>
+SoftwareSerial gsm(4,5);
+String outMessage = "Fall occured";
+String destinationNumber = "+918848257619";
+
+const int MPU_addr=0x68;  // I2C address of the MPU-6050
+float AcX;
+float Xpos,dx;
+void setup(){
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  Serial.begin(9600);
+  gsm.begin(9600); 
+}
+void loop(){
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  Xpos=Xpos*0.9+AcX*0.1;
+   dx=abs(AcX-Xpos);
+   if(dx>20000)
+   {
+        gsm.print("AT+CMGF=1\r");
+        delay(1000);
+        gsm.println("AT + CMGS = \"" + destinationNumber +"\"");
+        delay(1000);
+        gsm.print(outMessage);
+        delay(1000);
+        gsm.write((char)26); //ctrl+z
+        delay(1000);
+        gsm.println("AT+CLTS=1");
+        gsm.println("AT+CCLK?");
+
+   }
+  delay(333);
+  
+}
